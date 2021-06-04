95d9c54140b50cb704decb4cf028735d95e5081c
younghyeong ryu <wangta69@naver.com>
2021-06-04 18:58:59 +0900

    Initial commit

---
+++ node/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const webpush = require('web-push')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+
+const PUBLIC_VAPID = 'BLFfErGp4NfSrqmEdSkWacLoiEQ2ihfUAkwW6GW1rQFj0OkDcnIivTJd2KNL0UtG8KdNCQCa0-r9OBYsxV7aqFs'
+const PRIVATE_VAPID = '48ejva55CCiM5omPLqf30BiDEnYEs4WF1NmoWMxBvk8'
